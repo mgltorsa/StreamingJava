382ba5bbadedb98c95617c7b8ec8e13de613f308
miguelito657 <mglTorsa@gmail.com>
2018-11-05 00:39:11 -0500

    added ssl to server and client

--- Streaming/src/main/java/com/model/webclient/QueriesProxy.java
+++ Streaming/src/main/java/com/model/webclient/QueriesProxy.java
@@ -2,18 +2,28 @@ package com.model.webclient;
 
 import java.net.Socket;
 
+import javax.net.ssl.SSLSocketFactory;
+
 import com.google.gson.JsonObject;
 import com.model.connection.ITCPListener;
 import com.model.connection.TCPConnection;
 
 public class QueriesProxy extends ServiceProxy implements ITCPListener {
 
+    public static final String TRUSTTORE_LOCATION = "C:/Users/Asus/Desktop/Desktop/clientTrusted.jks";
+
     private TCPConnection connection;
     private JsonObject json;
+    private boolean activateSSL;
 
-    public QueriesProxy(Client client, String host, int port) {
+    public QueriesProxy(Client client, String host, int port, boolean activateSSL) {
 	super(client, host, port);
 	json = new JsonObject();
+	this.activateSSL = activateSSL;
+	if (activateSSL) {
+	    System.setProperty("javax.net.ssl.trustStore", TRUSTTORE_LOCATION);
+
+	}
     }
 
     @Override
@@ -24,13 +34,17 @@ public class QueriesProxy extends ServiceProxy implements ITCPListener {
 
     private Socket openSocket() {
 	Socket socket = null;
-
 	try {
-	    socket = new Socket(getHost(), getPort());
-
+	    if (activateSSL) {
+		SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+		socket = sf.createSocket(getHost(), getPort());
+	    } else {
+		socket = new Socket(getHost(), getPort());
+	    }
 	} catch (Exception e) {
 	    e.printStackTrace();
 	}
+
 	return socket;
     }
 
@@ -41,7 +55,7 @@ public class QueriesProxy extends ServiceProxy implements ITCPListener {
     public void processInputClient(String data) {
 	if (data.equalsIgnoreCase("send")) {
 	    this.connection.onInputMessageData(json.toString(), this);
-	    json= new JsonObject();
+	    json = new JsonObject();
 	} else {
 	    String[] info = data.split(":");
 	    String property = info[0].trim();
--- Streaming/src/main/java/com/model/webserver/QueriesService.java
+++ Streaming/src/main/java/com/model/webserver/QueriesService.java
@@ -10,34 +10,41 @@ import javax.net.ssl.SSLServerSocketFactory;
 import com.model.connection.ITCPListener;
 import com.model.connection.TCPConnection;
 
-public class QueriesService extends Service implements ITCPListener{
+public class QueriesService extends Service implements ITCPListener {
+
+    public static final String KEYSTORE_LOCATION = "C:/Users/Asus/Desktop/Desktop/key.jks";
+    public static final String KEYSTORE_PASSWORD = "password";
 
     private HashMap<String, TCPConnection> connections;
     private boolean activeSSL;
 
     public QueriesService(Server server, int port, boolean activeSSL) {
 	super(server, port);
-	this.activeSSL=activeSSL;
-	connections=new HashMap<String, TCPConnection>();
+	this.activeSSL = activeSSL;
+	if (activeSSL) {
+	    System.setProperty("javax.net.ssl.keyStore", KEYSTORE_LOCATION);
+	    System.setProperty("javax.net.ssl.keyStorePassword", KEYSTORE_PASSWORD);
+	}
+	connections = new HashMap<String, TCPConnection>();
     }
 
     @Override
     public void startService() {
 	ServerSocket serverSocket = openServerSocket();
-	while(true) {
+	while (true) {
 	    try {
 		Socket socket = serverSocket.accept();
-		
+
 		TCPConnection connection = new TCPConnection(socket, this);
 		connections.put(connection.getAddress(), connection);
 		connection.start();
-		
+
 	    } catch (IOException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	    }
 	}
-	
+
     }
 
     private ServerSocket openServerSocket() {
@@ -59,8 +66,7 @@ public class QueriesService extends Service implements ITCPListener{
     }
 
     public void onInputMessageData(String data, ITCPListener callback) {
-	getServer().onInputMessageData(data,callback);	
+	getServer().onInputMessageData(data, callback);
     }
 
-  
 }
